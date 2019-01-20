ab26325a0769c3a813be05b8504f626aef5edb3a
water_fairy <995637517@qq.com>
2019-01-21 01:15:12 +0800

    添加 移动

--- app/src/main/java/com/xueduoduo/imagecrop/ImageCropView.java
+++ app/src/main/java/com/xueduoduo/imagecrop/ImageCropView.java
@@ -3,6 +3,8 @@ package com.xueduoduo.imagecrop;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -23,6 +25,8 @@ import android.widget.ImageView;
 public class ImageCropView extends AppCompatImageView implements LineDrawer.OnLineChangeListener {
     private static final String TAG = "imageCropView";
     private LineDrawer mLineDrawer;
+    private Paint mPaint;
+
 
     public ImageCropView(Context context) {
         super(context, null);
@@ -30,24 +34,37 @@ public class ImageCropView extends AppCompatImageView implements LineDrawer.OnLi
 
     public ImageCropView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        test();
         mLineDrawer = new LineDrawer(context.getResources().getDisplayMetrics());
         mLineDrawer.setOnLineChangeListener(this);
         mLineDrawer.setBounds(new RectF(50, 50, 300, 300));
     }
 
+    private void test() {
+        mPaint = new Paint();
+        mPaint.setTextSize(30);
+        mPaint.setColor(Color.RED);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        if (changed)
+            mLineDrawer.setMaxRect(right - left, bottom - top);
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         mLineDrawer.draw(canvas);
+        canvas.drawText(mLineDrawer.toString(), 10, 25, mPaint);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
-        Log.i(TAG, "onTouchEvent: " + event.getAction());
         boolean touchMovePath = mLineDrawer.isTouchMovePath(event);
-        Log.i(TAG, "onTouchEvent: " + touchMovePath);
+        Log.i(TAG, "onTouchEvent: " + touchMovePath + "  " + event.getAction());
         return true;
     }
 
--- app/src/main/java/com/xueduoduo/imagecrop/LineDrawer.java
+++ app/src/main/java/com/xueduoduo/imagecrop/LineDrawer.java
@@ -17,7 +17,9 @@ import android.view.MotionEvent;
 public class LineDrawer {
     private final DisplayMetrics mDisplayMetrics;//
     private float mDensity;
-    //边界
+    //最大边界
+    private RectF mMaxRect;
+    //移动边界
     private RectF mRectF;
     //线
     private Paint mLinePaint;
@@ -41,6 +43,9 @@ public class LineDrawer {
     private boolean isMoving;
     private MoveStyle moveStyle;
     private OnLineChangeListener onLineChangeListener;
+    //移动旧坐标
+    private float lastX;
+    private float lastY;
 
     public void setOnLineChangeListener(OnLineChangeListener onLineChangeListener) {
         this.onLineChangeListener = onLineChangeListener;
@@ -72,10 +77,10 @@ public class LineDrawer {
         mLineWidthOutside = (int) (2 * mDisplayMetrics.density);
         mPartMinWidth = (int) (8 * mDisplayMetrics.density);
         //最小宽高
-        minWidth = (int) (mDisplayMetrics.density * 11);
+        minWidth = (int) (mDisplayMetrics.density * 40);
         minHeight = minWidth;
-        circleRadius = (int) (mDensity * 2);
-        touchCircleRadius = (int) (mDensity * 5);
+        circleRadius = (int) (mDensity * 3);
+        touchCircleRadius = (int) (mDensity * 13);
     }
 
     public void setBounds(RectF rectF) {
@@ -99,6 +104,7 @@ public class LineDrawer {
         canvas.drawLine(mRectF.left + horMean, mRectF.top, mRectF.left + horMean, mRectF.bottom, mLinePaint);
         canvas.drawLine(mRectF.right - horMean, mRectF.top, mRectF.right - horMean, mRectF.bottom, mLinePaint);
         //画中间交叉圆 左上 右上 左下 右下
+        mAreaPath.reset();
         mAreaPath.addCircle(mRectF.left + horMean, mRectF.top + verMean, circleRadius, Path.Direction.CW);
         mAreaPath.addCircle(mRectF.right - horMean, mRectF.top + verMean, circleRadius, Path.Direction.CW);
         mAreaPath.addCircle(mRectF.left + horMean, mRectF.bottom - verMean, circleRadius, Path.Direction.CW);
@@ -124,17 +130,133 @@ public class LineDrawer {
                 if (!isMoving && MoveStyle.NO != (moveStyle = isInCornerOrLine(event.getX(), event.getY()))) {
                     isMoving = true;
                 }
+                lastX = event.getX();
+                lastY = event.getY();
+                onLineChangeListener.onLineChange(this);
                 break;
             case MotionEvent.ACTION_MOVE:
                 if (isMoving) {
                     //移动处理
+                    handleMove(event.getX(), event.getY());
                 }
                 break;
             case MotionEvent.ACTION_UP:
+                isMoving = false;
+                onLineChangeListener.onLineChange(this);
                 break;
         }
         return isMoving;
 
+    }
+
+    private void handleMove(float x, float y) {
+        float left = mRectF.left;
+        float top = mRectF.top;
+        float right = mRectF.right;
+        float bottom = mRectF.bottom;
+        //1-8 判断边缘(动1-2边)
+        //9 :all  判断移向的边缘(四边都要移动)
+
+        switch (moveStyle) {
+            case LEFT:
+                left = getLeft(x, left, right);
+                break;
+            case TOP:
+                top = getTop(y, top, bottom);
+                break;
+            case RIGHT:
+                right = getRight(x, left, right);
+                break;
+            case BOTTOM:
+                bottom = getBottom(y, top, bottom);
+                break;
+            case LT:
+                top = getTop(y, top, bottom);
+                left = getLeft(x, left, right);
+                break;
+            case RT:
+                top = getTop(y, top, bottom);
+                right = getRight(x, left, right);
+                break;
+            case LB:
+                left = getLeft(x, left, right);
+                bottom = getBottom(y, top, bottom);
+                break;
+            case RB:
+                right = getRight(x, left, right);
+                bottom = getBottom(y, top, bottom);
+                break;
+            case ALL:
+                //判断
+                float withTemp = right - left;
+                float heightTemp = bottom - top;
+                left += (x - lastX);
+                right += (x - lastX);
+                if (x < lastX && left < touchCircleRadius) {
+                    //到达左侧
+                    left = touchCircleRadius;
+                    right = touchCircleRadius + withTemp;
+                } else if (x > lastX && right > mMaxRect.right - touchCircleRadius) {
+                    //到达右侧
+                    right = mMaxRect.right - touchCircleRadius;
+                    left = right - withTemp;
+                }
+                top += (y - lastY);
+                bottom += (y - lastY);
+
+                if (y < lastY && top < touchCircleRadius) {
+                    //到达左侧
+                    top = touchCircleRadius;
+                    bottom = touchCircleRadius + heightTemp;
+                } else if (y > lastY && bottom > mMaxRect.bottom - touchCircleRadius) {
+                    //到达右侧
+                    bottom = mMaxRect.bottom - touchCircleRadius;
+                    top = bottom - heightTemp;
+                }
+                break;
+        }
+        mRectF.set(left, top, right, bottom);
+        lastX = x;
+        lastY = y;
+        onLineChangeListener.onLineChange(this);
+    }
+
+    private float getTop(float y, float top, float bottom) {
+
+        top += (y - lastY);
+        if (top < touchCircleRadius) top = touchCircleRadius;
+        if (bottom - top < minHeight) {
+            top = bottom - minHeight;
+        }
+        return top;
+    }
+
+    private float getBottom(float y, float top, float bottom) {
+        bottom += (y - lastY);
+        if (bottom > mMaxRect.bottom - touchCircleRadius)
+            bottom = mMaxRect.bottom - touchCircleRadius;
+        if (bottom - top < minHeight) {
+            bottom = top + minHeight;
+        }
+        return bottom;
+    }
+
+    private float getLeft(float x, float left, float right) {
+        left += (x - lastX);
+        if (left < touchCircleRadius) left = touchCircleRadius;
+        if (right - left < minWidth) {
+            left = right - minWidth;
+        }
+        return left;
+    }
+
+    private float getRight(float x, float left, float right) {
+        right += (x - lastX);
+        if (right > mMaxRect.right - touchCircleRadius) right = mMaxRect.right - touchCircleRadius;
+        if (right - left < minWidth) {
+            right = left + minWidth;
+        }
+        return right;
     }
 
     /**
@@ -145,8 +267,6 @@ public class LineDrawer {
      * @return
      */
     private MoveStyle isInCornerOrLine(float x, float y) {
-
-
         if (isInLineVer(mRectF.left, x)) {
             //左边线
             if (isInLineHor(mRectF.top, y)) {
@@ -162,8 +282,10 @@ public class LineDrawer {
                 return MoveStyle.RB;
             } else return MoveStyle.RIGHT;
         } else if (isInLineHor(mRectF.top, y)) {
+            //上边
             return MoveStyle.TOP;
         } else if (isInLineHor(mRectF.bottom, y)) {
+            //下边
             return MoveStyle.BOTTOM;
         }
         return MoveStyle.NO;
@@ -185,7 +307,6 @@ public class LineDrawer {
      */
     private boolean isInLineVer(float x, float x1) {
         return x1 >= x - touchCircleRadius && x1 <= x + touchCircleRadius;
-
     }
 
     private MoveStyle isInCircle(float x, float y) {
@@ -222,6 +343,13 @@ public class LineDrawer {
         return xTemp >= (x - touchCircleRadius) && xTemp <= (x + touchCircleRadius) && yTemp >= (y - touchCircleRadius) && yTemp <= (y + touchCircleRadius);
     }
 
+    public void setMaxRect(int width, int height) {
+        if (mMaxRect == null) {
+            mMaxRect = new RectF();
+        }
+        mMaxRect.set(0, 0, width, height);
+    }
+
     public interface OnLineChangeListener {
         void onLineChange(LineDrawer lineDrawer);
     }
@@ -229,17 +357,16 @@ public class LineDrawer {
 
     public enum MoveStyle {
         NO(0),
-        ALL(1),//整体移动
-
-        LEFT(2),//左边界
-        TOP(3),//上
-        RIGHT(4),//右
-        BOTTOM(5),//下
+        LEFT(1),//左边界
+        TOP(2),//上
+        RIGHT(3),//右
+        BOTTOM(4),//下
         //左上 右上 左下 右下
         LT(5),
         RT(6),
         LB(7),
-        RB(8);
+        RB(8),
+        ALL(9);//整体移动
 
         MoveStyle(int ni) {
             nativeInt = ni;
@@ -248,4 +375,8 @@ public class LineDrawer {
         final int nativeInt;
     }
 
+    @Override
+    public String toString() {
+        return "moving: " + isMoving + " ; bounds: (" + (int) mRectF.left + "," + (int) mRectF.top + "," + (int) mRectF.right + "," + (int) mRectF.bottom + ") ; x: " + (int) lastX + " y: " + (int) lastY;
+    }
 }
